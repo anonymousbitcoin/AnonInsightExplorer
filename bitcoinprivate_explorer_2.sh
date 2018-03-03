b0b347af652e49e892a50a5fa1a66d3e7bb7e3ea
J62 <j.owens@interbiznw.com>
2018-03-03 15:32:13 -0800

    btcpify

--- bitcoinprivate_explorer_2.sh
+++ bitcoinprivate_explorer_2.sh
@@ -11,17 +11,17 @@ nvm install v4
 # install ZeroMQ libraries
 sudo apt-get -y install libzmq3-dev
 
-# install bitcore (branched and patched from https://github.com/z-classic/bitcore-node-zclassic.git)
-npm install z-classic/bitcore-node-zclassic
+# install bitcore (branched and patched from )
+npm install BTCPrivate/bitcore-node-bitcoinprivate
 
 # create bitcore node
-./node_modules/bitcore-node-zclassic/bin/bitcore-node create zclassic-explorer
-cd zclassic-explorer
+./node_modules/bitcore-node-bitcoinprivate/bin/bitcore-node create bitcoinprivate-explorer
+cd bitcoinprivate-explorer
 
-# install patched insight api/ui (branched and patched from https://github.com/z-classic/insight-api-zclassic & https://github.com/z-classic/insight-ui-zclassic)
-../node_modules/bitcore-node-zclassic/bin/bitcore-node install z-classic/insight-api-zclassic z-classic/insight-ui-zclassic
+# install patched insight api/ui (branched and patched from https://github.com/BTCPrivate/insight-api-bitcoinprivate & https://github.com/BTCPrivate/insight-ui-bitcoinprivate)
+../node_modules/bitcore-node-bitcoinprivate/bin/bitcore-node install BTCPrivate/insight-api-bitcoinprivate BTCPrivate/insight-ui-bitcoinprivate
 
-# create bitcore config file for bitcore and zcashd/zclassicd
+# create bitcore config file for bitcore and btcpd
 # REPLACE "datadir" and "exec" with actual values of "/home/user"
 cat << EOF > bitcore-node.json
 {
@@ -29,21 +29,22 @@ cat << EOF > bitcore-node.json
   "port": 3001,
   "services": [
     "bitcoind",
-    "insight-api-zclassic",
-    "insight-ui-zclassic",
+    "insight-api-bitcoinprivate",
+    "insight-ui-bitcoinprivate",
     "web"
   ],
   "servicesConfig": {
     "bitcoind": {
       "spawn": {
-        "datadir": "/home/ubuntu/j62/.zclassic",
-        "exec": "/home/ubuntu/j62/zclassic/src/zcashd"
+        "datadir": "/home/ubuntu/.btcprivate",
+        "exec": "/home/ubuntu/BitcoinPrivate/src/btcpd"
       }
     },
-    "insight-ui-zclassic": {
-      "apiPrefix": "api"
+    "insight-ui-bitcoinprivate": {
+      "apiPrefix": "api",
+      "routePrefix": ""
     },
-    "insight-api-zclassic": {
+    "insight-api-bitcoinprivate": {
       "routePrefix": "api"
     }
   }
@@ -51,26 +52,8 @@ cat << EOF > bitcore-node.json
 
 EOF
 
-# create zcash.conf
-cat << EOF > data/zclassic.conf
-server=1
-whitelist=127.0.0.1
-txindex=1
-addressindex=1
-timestampindex=1
-spentindex=1
-zmqpubrawtx=tcp://127.0.0.1:28332
-zmqpubhashblock=tcp://127.0.0.1:28332
-rpcallowip=127.0.0.1
-rpcuser=bitcoin
-rpcpassword=local321
-uacomment=bitcore
-showmetrics=1
-maxconnections=1000
 
-
-EOF
 
 echo "Start the block explorer, open in your browser http://server_ip:3001"
 echo "Run the following line as one line of commands to start the block explorer"
-echo "nvm use v4; cd zclassic-explorer; ./node_modules/bitcore-node-zclassic/bin/bitcore-node start"
+echo "nvm use v4; cd bitcoinprivate-explorer; ./node_modules/bitcore-node-bitcoinprivate/bin/bitcore-node start"
